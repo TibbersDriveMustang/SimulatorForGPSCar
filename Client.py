124c3a0120949a6a46e09773e1b815aac2c8207e
Hongyi Guo <guohongyi1991@gmail.com>
2016-04-05 01:15:30 -0500

    First Commit

---
+++ Client.py
@@ -0,0 +1,53 @@
+__author__ = 'Tibbers'
+from Tkinter import *
+import tkMessageBox
+from PIL import Image,ImageTk
+import socket, threading, sys, traceback, os
+
+class Client:
+
+
+    #Initialization
+    def __init__(self, master, server_addr, server_port):
+        self.master = master
+        self.master.protocol("WM_DELETE_WINDOW",self.handler)
+        self.createWidgets()
+        self.server_addr = server_addr
+        self.server_port = server_port
+        self.connectToServer()
+
+
+    """def createWidgets(self):
+        "Build GUI"
+
+        #Create Setup button
+        self.setup = Button(self.master, width = 20, padx = 3, pady = 3)
+        self.setup["text"] =  "Setup"
+        self.setup["command"] = self.setupTCP
+        ....
+
+    """
+    def connectTOServer(self):
+        "Connect to server"
+        self.TCPSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        try:
+            self.TCPSocket.connect((self.server_addr, self.server_port))
+        except:
+            tkMessageBox.showwarning('Connection Failed', 'Connection to \'%s\' failed.' %self.server_addr)
+
+
+if __name__ == "__main__":
+	try:
+		serverAddr = sys.argv[1]
+		serverPort = sys.argv[2]
+	except:
+		print "[Usage: Client.py Server_IP Server_port]\n"
+
+	root = Tk()
+
+	# Create a new client
+	#app = Client(root, serverAddr, serverPort, rtpPort, fileName)
+	app = Client(root,serverAddr,serverPort)
+	#app.master.title("RTPClient")
+	app.master.title("PythonClient")
+	root.mainloop()
---
+++ ClientLauncher.py
@@ -0,0 +1,7 @@
+__author__ = 'Tibbers'
+import sys
+from Tkinter import Tk
+from Client import Client
+
+if __name__ == "__main__":
+    try:
